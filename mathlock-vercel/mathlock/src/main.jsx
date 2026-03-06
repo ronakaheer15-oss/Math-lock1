7aa3ccc7cfdfe9bb4fe25b7bc7e6c8d40b2c35bb
Ronak Aheer <ronakaheer15@gmail.com>
2026-03-06 15:50:54 +0530

    Delete mathlock-vercel/mathlock directory

--- mathlock-vercel/mathlock/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MathLock from './MathLock'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MathLock />
-  </React.StrictMode>,
-)
